cc3d98574ccad77fcbf43dc6d7b3ff15d8968a45
Abhijithps22 <abhijithabi22102001@gmail.com>
2023-05-27 16:17:45 +0530

    dietitian , delivery man

--- src/components/router/switch/index.js
+++ src/components/router/switch/index.js
@@ -35,6 +35,8 @@ import Banner from "../../private/pages/banner/banner";
 import BannerType from "../../private/pages/banner/bannerType";
 import MealIngredient from "../../private/pages/mealSettings/mealIngredient";
 import RecipeIngredient from "../../private/pages/mealSettings/recipeIngredient";
+import Dietician from "../../private/pages/dietician";
+import Deliveryman from "../../private/pages/dispatch/deliveryman";
 /**
  * Switch component to render different pages based on the provided page prop.
  * @param {string} page - The page to be rendered.
@@ -357,6 +359,24 @@ const Switch = ({
     case "recipe-ingredient":
       return (
         <RecipeIngredient
+          key={key}
+          addPrivilege={addPrivilege}
+          delPrivilege={delPrivilege}
+          updatePrivilege={updatePrivilege}
+        />
+      );
+    case "dietitian":
+      return (
+        <Dietician
+          key={key}
+          addPrivilege={addPrivilege}
+          delPrivilege={delPrivilege}
+          updatePrivilege={updatePrivilege}
+        />
+      );
+    case "delivery-man":
+      return (
+        <Deliveryman
           key={key}
           addPrivilege={addPrivilege}
           delPrivilege={delPrivilege}
