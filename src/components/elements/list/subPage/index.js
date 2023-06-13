22ede6a6a75db5bc01b6895dc51ded515035d742
azharhashim1992 <35956918+azharhashim1992@users.noreply.github.com>
2023-06-14 00:28:02 +0530

    update now

--- src/components/elements/list/subPage/index.js
+++ src/components/elements/list/subPage/index.js
@@ -5,13 +5,13 @@ import { Footer, Header, Overlay, Page } from "./../manage/styles";
 import { getValue } from "../functions";
 const SubPage = ({ subAttributes, setLoaderBox, setMessage, closeModal, itemTitle }) => {
   const [t] = useTranslation();
-  const titleValue = (itemTitle.collection?.length > 0 ? (subAttributes?.data?.[itemTitle.collection]?.[itemTitle.name] ?? '') : subAttributes?.data?.[itemTitle.name]) || "Please update the itemTitle | - ItemTitle: Give item title for List Item Table inside each page. This array name should be there inside the array.";
-
+  const titleValue = (itemTitle.collection?.length > 0 ? subAttributes?.data?.[itemTitle.collection]?.[itemTitle.name] ?? "" : subAttributes?.data?.[itemTitle.name]) || "Please update the itemTitle | - ItemTitle: Give item title for List Item Table inside each page. This array name should be there inside the array.";
+  const viewMode = subAttributes.item.type ?? "subList";
   return (
     <Overlay>
       <Page className={subAttributes?.item?.params?.customClass ?? ""}>
         <Header>{`${getValue(itemTitle, titleValue)} / ${t(subAttributes?.item?.title)}`}</Header>
-        <ListTable setMessage={setMessage} setLoaderBox={setLoaderBox} parentReference={subAttributes?.item?.params?.parentReference} referenceId={subAttributes?.data?._id} attributes={subAttributes.item.attributes} {...subAttributes.item.params}></ListTable>
+        <ListTable viewMode={viewMode} setMessage={setMessage} setLoaderBox={setLoaderBox} parentReference={subAttributes?.item?.params?.parentReference} referenceId={subAttributes?.data?._id} attributes={subAttributes.item.attributes} {...subAttributes.item.params}></ListTable>
         <Footer>
           <FormInput type="close" value={"Cancel"} onChange={closeModal} />
         </Footer>
