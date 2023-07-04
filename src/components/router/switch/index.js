96fa3e9de29ec21f6f50465c5c784d976a6bb6b2
kavyavishnuhari <kavyarameshbabu1999@gmail.com>
2023-07-04 18:03:47 +0530

    Edits: Blog, Subscriber, AdmissionHistory, ActiveAdmission, ActiveAppointment,AppointmentHistory and Submenu created in Order: TomorrowOrder are completed.

--- src/components/router/switch/index.js
+++ src/components/router/switch/index.js
@@ -57,6 +57,7 @@ import PageSettings from "../../private/pages/settings/pageSettings";
 // import WeeklyMealPlanEntry from "../../private/pages/mealSettings/weeklyMealPlanEntry";
 import Admin from "../../private/pages/franchise/admin";
 import Student from "../../private/pages/user/student/student";
+import TomorrowOrder from "../../private/pages/order/tomorrowOrder";
 /**
  * Switch component to render different pages based on the provided page prop.
  * @param {string} page - The page to be rendered.
@@ -629,6 +630,16 @@ const Switch = ({
     case "students":
       return (
         <Student
+          key={key}
+          exportPrivilege={exportPrivilege}
+          addPrivilege={addPrivilege}
+          delPrivilege={delPrivilege}
+          updatePrivilege={updatePrivilege}
+        />
+      );
+    case "tomorrow-order":
+      return (
+        <TomorrowOrder
           key={key}
           exportPrivilege={exportPrivilege}
           addPrivilege={addPrivilege}
