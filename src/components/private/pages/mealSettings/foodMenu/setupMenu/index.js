8448d5570e5f0954d7d29adb9e3262033ff2436c
saleekhaabbas <saleekha.melethil@gmail.com>
2023-07-22 21:02:26 +0530

    working with recipe

--- src/components/private/pages/mealSettings/foodMenu/setupMenu/index.js
+++ src/components/private/pages/mealSettings/foodMenu/setupMenu/index.js
@@ -2,15 +2,43 @@ import React, { useEffect, useState } from "react";
 import { GetIcon } from "../../../../../../icons";
 import { getData, postData } from "../../../../../../backend/api";
 import { NoData } from "../../../../../elements/list/styles";
-import { ColumnContainer, RowContainer } from "../../../../../styles/containers/styles";
+import {
+  ColumnContainer,
+  RowContainer,
+} from "../../../../../styles/containers/styles";
 import Search from "../../../../../elements/search";
-import { TabContainer, TabButton, Table, TableHeader, TableBody, TableRow, MealCategoryCell, Div, TableCell, FoodButton, TabData, TabDataItem, MealItem, Title, Variants, Variant } from "./styles"; // Import styles from styles.js
+import {
+  TabContainer,
+  TabButton,
+  Table,
+  TableHeader,
+  TableBody,
+  TableRow,
+  MealCategoryCell,
+  Div,
+  TableCell,
+  FoodButton,
+  TabData,
+  TabDataItem,
+  MealItem,
+  Title,
+  Variants,
+  Variant,
+} from "./styles"; // Import styles from styles.js
 import DraggableItem from "./dragdrop/drag";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { DndProvider } from "react-dnd";
 import DropTarget from "./dragdrop/drop";
 const SetupMenu = ({ openData, themeColors }) => {
-  const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
   const [menuId] = useState(openData.data._id);
   const [menuData, setMenuData] = useState(null);
   const [meals, setMeals] = useState([]);
@@ -23,31 +51,57 @@ const SetupMenu = ({ openData, themeColors }) => {
 
   const handleTabClick = (tab) => {
     setActiveTab(tab);
-    getData({}, tab === "meals" ? "food-group/search" : "recipe/search").then((result) => {
-      tab === "meals" ? setMeals(result.data.response) : setRecipes(result.data.response);
-      console.log("meals", meals, "recipes", recipes);
-    });
+    getData({}, tab === "meals" ? "food-group/search" : "recipe/search").then(
+      (result) => {
+        tab === "meals"
+          ? setMeals(result.data.response)
+          : setRecipes(result.data.response);
+        console.log("meals", meals, "recipes", recipes);
+      }
+    );
   };
   const handleAddFood = (mealTimeCategoryId, dayIndex) => {
     // Handle adding food to the selected cell
-    console.log(`Adding food to Meal Time Category ${mealTimeCategoryId} on Day ${dayIndex} to ${menuId}`);
-    alert(`Adding food to Meal Time Category ${mealTimeCategoryId} on Day ${dayIndex} to ${menuId}`);
+    console.log(
+      `Adding food to Meal Time Category ${mealTimeCategoryId} on Day ${dayIndex} to ${menuId}`
+    );
+    alert(
+      `Adding food to Meal Time Category ${mealTimeCategoryId} on Day ${dayIndex} to ${menuId}`
+    );
   };
   const onDrop = async (item, data) => {
     const menuDataTemp = { ...menuData };
-    const items = menuDataTemp.foodMenu.find((cat) => data.mealTimeCategory === cat.mealTimeCategory && data.dayNumber === cat.dayNumber);
+    const items = menuDataTemp.foodMenu.find(
+      (cat) =>
+        data.mealTimeCategory === cat.mealTimeCategory &&
+        data.dayNumber === cat.dayNumber
+    );
     if (items) {
       items.recipeVariants.push({ ...item, ...data });
     } else {
-      menuDataTemp.foodMenu.push({ ...data, meals: [], recipeVariants: [{ ...item, ...data }] });
+      menuDataTemp.foodMenu.push({
+        ...data,
+        meals: [],
+        recipeVariants: [{ ...item, ...data }],
+      });
     }
     setMenuData(menuDataTemp);
-    await postData({ ...data, mealOrRecepe: item.mealOrRecepe, foodMenu: menuId, recipeVariant: item._id }, "food-menu-item");
+    await postData(
+      {
+        ...data,
+        mealOrRecepe: item.mealOrRecepe,
+        foodMenu: menuId,
+        recipeVariant: item._id,
+      },
+      "food-menu-item"
+    );
   };
   useEffect(() => {
-    getData({ menuId: openData.data._id }, "food-menu/get-a-menu").then((response) => {
-      setMenuData(response.data);
-    });
+    getData({ menuId: openData.data._id }, "food-menu/get-a-menu").then(
+      (response) => {
+        setMenuData(response.data);
+      }
+    );
   }, [openData.data._id]);
 
   return menuData ? (
@@ -66,15 +120,27 @@ const SetupMenu = ({ openData, themeColors }) => {
             <TableBody>
               {menuData.mealTimeCategories.map((mealTimeCategory) => (
                 <TableRow key={mealTimeCategory._id}>
-                  <MealCategoryCell>{mealTimeCategory.mealtimeCategoriesName}</MealCategoryCell>
+                  <MealCategoryCell>
+                    {mealTimeCategory.mealtimeCategoriesName}
+                  </MealCategoryCell>
                   {daysOfWeek.map((day, dayNumber) => {
-                    const items = menuData.foodMenu.find((item) => item.mealTimeCategory === mealTimeCategory._id && item.dayNumber === dayNumber);
+                    const items = menuData.foodMenu.find(
+                      (item) =>
+                        item.mealTimeCategory === mealTimeCategory._id &&
+                        item.dayNumber === dayNumber
+                    );
                     return (
-                      <TableCell className={dayNumber === 0 ? "first" : ""} key={dayNumber}>
+                      <TableCell
+                        className={dayNumber === 0 ? "first" : ""}
+                        key={dayNumber}
+                      >
                         <Div>
                           <DropTarget
                             onDrop={onDrop}
-                            data={{ mealTimeCategory: mealTimeCategory._id, dayNumber }}
+                            data={{
+                              mealTimeCategory: mealTimeCategory._id,
+                              dayNumber,
+                            }}
                             element={
                               <Variants className="vertical">
                                 {items?.recipeVariants?.length > 0 ? (
@@ -83,13 +149,24 @@ const SetupMenu = ({ openData, themeColors }) => {
                                     // For example, you can render a list of items like this
                                     return (
                                       <Variant className="vertical">
-                                        <span className="recipe">{item.recipe.title} </span>
-                                        <span className="variant">{item.variant} </span>
+                                        <span className="recipe">
+                                          {item.recipe.title}{" "}
+                                        </span>
+                                        <span className="variant">
+                                          {item.variant}{" "}
+                                        </span>
                                       </Variant>
                                     );
                                   })
                                 ) : (
-                                  <FoodButton onClick={() => handleAddFood(mealTimeCategory._id, dayNumber)}>
+                                  <FoodButton
+                                    onClick={() =>
+                                      handleAddFood(
+                                        mealTimeCategory._id,
+                                        dayNumber
+                                      )
+                                    }
+                                  >
                                     <GetIcon icon={"add"}></GetIcon>
                                   </FoodButton>
                                 )}
@@ -108,16 +185,28 @@ const SetupMenu = ({ openData, themeColors }) => {
         </RowContainer>
         <RowContainer className="mealSelection">
           <TabContainer>
-            <TabButton active={activeTab === "meals"} onClick={() => handleTabClick("meals")}>
+            <TabButton
+              active={activeTab === "meals"}
+              onClick={() => handleTabClick("meals")}
+            >
               Meals
             </TabButton>
-            <TabButton active={activeTab === "recipes"} onClick={() => handleTabClick("recipes")}>
+            <TabButton
+              active={activeTab === "recipes"}
+              onClick={() => handleTabClick("recipes")}
+            >
               Recipes
             </TabButton>
           </TabContainer>
 
           <TabData>
-            <Search title={"Search"} theme={themeColors} placeholder="Search" value={searchValue} onChange={searchChange} />
+            <Search
+              title={"Search"}
+              theme={themeColors}
+              placeholder="Search"
+              value={searchValue}
+              onChange={searchChange}
+            />
             {activeTab === "meals" && meals && (
               <TabDataItem>
                 {meals.map((meal) => (
@@ -126,7 +215,11 @@ const SetupMenu = ({ openData, themeColors }) => {
                   // </MealItem>
                   <DraggableItem
                     key={meal._id}
-                    item={{ ...meal, mealOrRecepe: "meal", meal: { title: meal.title } }}
+                    item={{
+                      ...meal,
+                      mealOrRecepe: "meal",
+                      meal: { title: meal.title },
+                    }}
                     element={
                       <MealItem key={meal._id}>
                         <Title>{meal.title ?? "Title not found!"}</Title>
@@ -137,10 +230,16 @@ const SetupMenu = ({ openData, themeColors }) => {
                               <Variant key={item._id}>
                                 <span>
                                   <span>BHD</span>
-                                  <span className="price">{recipeVariant.price}</span>
-                                  <span className="offer">{recipeVariant.offerPrice}</span>{" "}
+                                  <span className="price">
+                                    {recipeVariant.price}
+                                  </span>
+                                  <span className="offer">
+                                    {recipeVariant.offerPrice}
+                                  </span>{" "}
                                 </span>
-                                <span className="variant">{recipeVariant.variant}</span>
+                                <span className="variant">
+                                  {recipeVariant.variant}
+                                </span>
                               </Variant>
                             );
                           })}
@@ -161,15 +260,23 @@ const SetupMenu = ({ openData, themeColors }) => {
                         return (
                           <DraggableItem
                             key={variant._id}
-                            item={{ ...variant, mealOrRecepe: "recipe", recipe: { title: recipe.title } }}
+                            item={{
+                              ...variant,
+                              mealOrRecepe: "recipe",
+                              recipe: { title: recipe.title },
+                            }}
                             element={
                               <Variant>
                                 <span>
                                   <span>BHD</span>
                                   <span className="price">{variant.price}</span>
-                                  <span className="offer">{variant.price}</span>{" "}
+                                  <span className="offer">
+                                    {variant.price}
+                                  </span>{" "}
                                 </span>
-                                <span className="variant">{variant.variant} </span>
+                                <span className="variant">
+                                  {variant.variant}{" "}
+                                </span>
                               </Variant>
                             }
                           />
