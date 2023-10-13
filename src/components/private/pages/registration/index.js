e30bfeb1d33a0c0ab8fbc15436cbb31c106c7dce
ADHIL K P <103945365+356Adhil@users.noreply.github.com>
2023-10-13 11:22:42 +0530

    added export option and listed event attended users

--- src/components/private/pages/registration/index.js
+++ src/components/private/pages/registration/index.js
@@ -5,177 +5,174 @@ import { Container } from "../../common/layout/styels";
 //src/components/styles/page/index.js
 //if you want to write custom style wirte in above file
 const Registration = (props) => {
-    //to update the page title
-    useEffect(() => {
-        document.title = `Registration - DataHex SIO Kerala Portal`;
-    }, []);
+  //to update the page title
+  useEffect(() => {
+    document.title = `Registration - DataHex SIO Kerala Portal`;
+  }, []);
 
-    const [attributes] = useState([
-        {
-            type: "text",
-            placeholder: "Name",
-            name: "name",
-            validation: "",
-            default: "",
-            label: "Name",
-            // tag: true,
-            required: true,
-            view: true,
-            add: true,
-            update: true,
-        },
-        {
-            type: "number",
-            placeholder: "Mobile Number",
-            name: "mobileNumber",
-            validation: "",
-            default: "",
-            tag: true,
-            label: "Mobile Number",
-            required: true,
-            view: true,
-            add: true,
-            update: true,
-        },
-        {
-            type: "email",
-            placeholder: "Email",
-            name: "email",
-            validation: "",
-            default: "",
-            label: "Email",
-            tag: true,
-            required: true,
-            view: true,
-            add: true,
-            update: true,
-        },
-        {
-            type: "password",
-            placeholder: "Password",
-            name: "password",
-            validation: "",
-            default: "",
-            // tag: true,
-            label: "Password",
-            required: true,
-            view: false,
-            add: true,
-            update: true,
-        },
-        {
-            type: "select",
-            placeholder: "Gender",
-            name: "gender",
-            validation: "",
-            default: "",
-            tag: true,
-            label: "Gender",
-            showItem: "Gender",
-            required: false,
-            view: true,
-            filter: false,
-            add: true,
-            update: true,
-            apiType: "CSV",
-            selectApi: "Male,Female",
-        },
-        {
-            type: "date",
-            placeholder: "Date Of Birth",
-            name: "dateOfBirth",
-            validation: "",
-            default: "",
-            label: "Date Of Birth",
-            tag: true,
-            required: false,
-            view: true,
-            add: true,
-            update: true,
-        },
-        {
-            type: "text",
-            placeholder: "Place",
-            name: "place",
-            validation: "",
-            default: "",
-            tag: true,
-            label: "Place",
-            required: false,
-            view: true,
-            add: true,
-            update: true,
-        },
-        {
-            type: "text",
-            placeholder: "District",
-            name: "district",
-            validation: "",
-            default: "",
-            tag: true,
-            label: "District",
-            required: false,
-            view: true,
-            add: true,
-            update: true,
-        },
-        {
-            type: "text",
-            placeholder: "Profession",
-            name: "profession",
-            validation: "",
-            default: "",
-            tag: true,
-            label: "Profession",
-            required: false,
-            view: true,
-            add: true,
-            update: true,
-        },
+  const [attributes] = useState([
+    {
+      type: "text",
+      placeholder: "Name",
+      name: "name",
+      validation: "",
+      default: "",
+      label: "Name",
+      // tag: true,
+      required: true,
+      view: true,
+      add: true,
+      update: true,
+    },
+    {
+      type: "number",
+      placeholder: "Mobile Number",
+      name: "mobileNumber",
+      validation: "",
+      default: "",
+      tag: true,
+      label: "Mobile Number",
+      required: true,
+      view: true,
+      add: true,
+      update: true,
+    },
+    {
+      type: "email",
+      placeholder: "Email",
+      name: "email",
+      validation: "",
+      default: "",
+      label: "Email",
+      tag: true,
+      required: true,
+      view: true,
+      add: true,
+      update: true,
+    },
+    {
+      type: "password",
+      placeholder: "Password",
+      name: "password",
+      validation: "",
+      default: "",
+      // tag: true,
+      label: "Password",
+      required: true,
+      view: false,
+      add: true,
+      update: true,
+    },
+    {
+      type: "select",
+      placeholder: "Gender",
+      name: "gender",
+      validation: "",
+      default: "",
+      tag: true,
+      label: "Gender",
+      showItem: "Gender",
+      required: false,
+      view: true,
+      filter: false,
+      add: true,
+      update: true,
+      apiType: "CSV",
+      selectApi: "Male,Female",
+    },
+    {
+      type: "text",
+      placeholder: "Place",
+      name: "place",
+      validation: "",
+      default: "",
+      tag: true,
+      label: "Place",
+      required: false,
+      view: true,
+      add: true,
+      update: true,
+    },
+    {
+      type: "text",
+      placeholder: "District",
+      name: "district",
+      validation: "",
+      default: "",
+      tag: true,
+      label: "District",
+      required: false,
+      view: true,
+      add: true,
+      update: true,
+    },
+    {
+      type: "text",
+      placeholder: "Profession",
+      name: "profession",
+      validation: "",
+      default: "",
+      tag: true,
+      label: "Profession",
+      required: false,
+      view: true,
+      add: true,
+      update: true,
+    },
+    {
+      type: "date",
+      placeholder: "Registration Date",
+      name: "regDate",
+      validation: "",
+      default: "",
+      label: "Registration Date",
+      tag: true,
+      required: false,
+      view: true,
+      add: true,
+      update: true,
+    },
+    {
+      type: "text",
+      placeholder: "Institution",
+      name: "institution",
+      validation: "",
+      default: "",
+      tag: true,
+      label: "Institution",
+      required: false,
+      view: true,
+      add: true,
+      update: true,
+    },
+    {
+      type: "image",
+      placeholder: "QR Code",
+      name: "qrImageUrl",
+      validation: "",
+      default: "false",
+      tag: true,
+      label: "QR Code",
+      required: false,
+      view: true,
+      add: true,
+      update: true,
+    },
+  ]);
 
-        {
-            type: "image",
-            placeholder: "QR Code",
-            name: "qrImageUrl",
-            validation: "",
-            default: "false",
-            tag: true,
-            label: "QR Code",
-            required: false,
-            view: true,
-            add: true,
-            update: true,
-        },
-
-         {
-            type: "text",
-            placeholder: "Institution",
-            name: "institution",
-            validation: "",
-            default: "",
-            tag: true,
-            label: "Institution",
-            required: false,
-            view: true,
-            add: true,
-            update: true,
-        },
-
-    ]);
-
-    return (
-        <Container className="noshadow">
-            <ListTable
-                // actions={actions}
-                api={`registration`}
-                itemTitle={{ name: "name", type: "text", collection: "" }}
-                shortName={`Registration`}
-                formMode={`double`}
-                {...props}
-                attributes={attributes}
-            ></ListTable>
-        </Container>
-    );
+  return (
+    <Container className="noshadow">
+      <ListTable
+        // actions={actions}
+        api={`registration`}
+        itemTitle={{ name: "name", type: "text", collection: "" }}
+        shortName={`Registration`}
+        formMode={`double`}
+        {...props}
+        attributes={attributes}
+      ></ListTable>
+    </Container>
+  );
 };
 // exporting the page with parent container layout..
 export default Layout(Registration);
